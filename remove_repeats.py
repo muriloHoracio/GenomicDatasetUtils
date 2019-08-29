b8bfab8fc927c837db3ed3582aa4e454ba64946a
Murilo Cruz <murilo.horacio@gmail.com>
2019-08-29 05:02:43 -0300

    Keeping headers while removing repeats

--- remove_repeats.py
+++ remove_repeats.py
@@ -6,39 +6,42 @@ repeated = ''
 out = ''
 seqs = dict()
 
-def check_repeat(fl,header,seq):
-	if seq in seqs:
-		repeated += fl + '\t' + seqs[fl][seq].strip() + '\t' + fl + '\t' + header.strip() + '\n'
-	seqs[seq] = header
-
 for fl in fls:
 	with open(fl,'r') as f:
+		seqs[fl] = dict()
 		seq = ''
-		for l in fl.readlines():
+		for l in f.readlines():
 			if l[0] == '>':
 				if seq != '':
-					check_repeat(fl,header,seq)
+					seq = seq.upper()
+					if seq in seqs:
+						repeated += fl + '\t' + seqs[fl][seq][0].strip() + '\t' + fl + '\t' + header.strip() + '\n'
+					seqs[fl][seq] = [header, True]
 				header = l
 				seq = ''
 			else:
 				seq += l
-		check_repeat(fl,header,seq)
+		seq = seq.upper()
+		if seq in seqs:
+			repeated += fl + '\t' + seqs[fl][seq][0].strip() + '\t' + fl + '\t' + header.strip() + '\n'
+		seqs[fl][seq] = [header, True]
 
 for i in range(len(fls)):
 	for s in seqs[fls[i]]:
 		for j in range(len(fls)):
 			if i != j:
 				if s in seqs[fls[j]]:
-					repeated += fls[i] + '\t' + seqs[fls[i]][s].strip() + '\t' + fls[j] + '\t' + seqs[fls[j]][s].strip() + '\n'
+					repeated += fls[i] + '\t' + seqs[fls[i]][s][0].strip() + '\t' + fls[j] + '\t' + seqs[fls[j]][s][0].strip() + '\n'
 					if len(seqs[fls[i]]) > len(seqs[fls[j]]):
-						seqs[fls[i]].pop(s,None)
+						seqs[fls[i]][s][1] = False
 					else:
-						seqs[fls[j]].pop(s,None)
+						seqs[fls[j]][s][1] = False
 
 for fl in fls:
 	out = ''
 	for s in seqs[fl]:
-		out += seqs[fl][s] + s
+		if seqs[fl][s][1] == True:
+			out += seqs[fl][s][0] + s
 	with open(fl.split('.')[0]+'_UNIQUES.tsv','w+') as f:
 		f.write(out)
 
