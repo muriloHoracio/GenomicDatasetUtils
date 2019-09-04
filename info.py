98105020db98f80f04986cbc48bafc91c0e2db6e
Murilo Cruz <murilo.horacio@gmail.com>
2019-09-03 21:29:52 -0300

    Correct the output

--- info.py
+++ info.py
@@ -5,6 +5,8 @@ repeats = []
 dic = dict()
 dbs = dict()
 
+_bases = ["DPTE","PGSB","REPBASE","RiTE","SPTE","TEfam","TREP"]
+
 bases = {
 	"DPTE": 0,
 	"PGSB": 0,
@@ -14,6 +16,8 @@ bases = {
 	"TEfam": 0,
 	"TREP": 0
 }
+
+_superfamilies = ["Copia", "Gypsy","Bel-Pao","Retrovirus","ERV","DIRS","Ngaro","VIPER","Penelope","R2","RTE","Jockey","LINE","L1","I","SINE","tRNA","7SL","5S","Mariner","hAT","Mutator","Merlin","Transib","P","PiggyBac","PIF","CACTA","Crypton","Helitron","Maverick","MITE","TRIM"]
 
 superfamilies = {
 	"Copia": dict(bases), 
@@ -58,15 +62,15 @@ for db in [d for d in os.listdir('.') if os.path.isdir(d)]:
 				if l[0] == '>':
 					superfamilies[fl.split('.')[0]][db] += 1
 header = 'Superfamily'
-for db in bases:
+for db in _bases:
 	header += ','+db
 header += '\n'
 
 out = ''
-for s in superfamilies:
+for s in _superfamilies:
 	out += s
 	_sum = 0
-	for db in bases:
+	for db in _bases:
 		_sum += superfamilies[s][db]
 		out += ','+str(superfamilies[s][db])
 	out +=','+str(_sum)+'\n'
@@ -75,4 +79,3 @@ out = header + out
 
 with open('seqs.csv','w+') as f:
 	f.write(out)
-
