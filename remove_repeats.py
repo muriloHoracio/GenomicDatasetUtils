428272d555081bca8acecae4503211ad9740d841
Murilo Horacio Pereira da Cruz <murilo.horacio@gmail.com>
2019-09-23 20:27:19 -0300

    Correct the repeat checking proccess by adding stripped sequences to the dictionary

--- remove_repeats.py
+++ remove_repeats.py
@@ -19,91 +19,48 @@ for db in [d for d in os.listdir('.') if os.path.isdir(d)]:
 					if seq != '':
 						seq = clr_seq.subn('N',seq.upper())[0]
 						if seq in seqs[db][fl]:
-							repeated += db + '\t' + fl + '\t' + header.strip() + '\t' + db + '\t' + fl + '\t' + seqs[db][fl][seq][0].strip() + '\n'
+							repeated += db + '\t' + fl + '\t' + header.strip() + '\t|<>|\t' + db + '\t' + fl + '\t' + seqs[db][fl][seq][0].strip() + '\n'
 						seqs[db][fl][seq] = [header, True]
 					header = l
 					seq = ''
 				else:
-					seq += l
+					seq += l.strip()
 			seq = clr_seq.subn('N',seq.upper())[0]
 			if seq in seqs[db][fl]:
-				repeated += db + '\t' + fl + '\t' + header.strip() + '\t' + db + '\t' + fl + '\t' + seqs[db][fl][seq][0].strip() + '\n'
+				repeated += db + '\t' + fl + '\t' + header.strip() + '\t|<>|\t' + db + '\t' + fl + '\t' + seqs[db][fl][seq][0].strip() + '\n'
 			seqs[db][fl][seq] = [header, True]
 
+seqs_amount = dict()
+for db in seqs:
+	seqs_amount[db] = dict()
+	for fl in seqs[db]:
+		seqs_amount[db][fl] = len(seqs[db][fl])
 
 for db in seqs:
 	for fl in seqs[db]:
 		for s in seqs[db][fl]:
 			for _db in seqs:
-				#for _fl in [f for f in seqs[_db] if f != fl]:
 				for _fl in seqs[_db]:
 					if not (db == _db and fl == _fl):
 						if s in seqs[_db][_fl] and seqs[_db][_fl][s][1]:
-							repeated += db + '\t' + fl + '\t' + seqs[db][fl][s][0].strip() + '\t' + _db + '\t' + _fl + '\t' + seqs[_db][_fl][s][0].strip() + '\n'
-							if len(seqs[db][fl]) > len(seqs[_db][_fl]):
+							repeated += db + '\t' + fl + '\t' + seqs[db][fl][s][0].strip() + '\t|<>|\t' + _db + '\t' + _fl + '\t' + seqs[_db][_fl][s][0].strip() + '\n'
+							if seqs_amount[db][fl] > seqs_amount[_db][_fl]:
 								seqs[db][fl][s][1] = False
+								seqs_amount[db][fl] += -1
 							else:
 								seqs[_db][_fl][s][1] = False
+								seqs_amount[_db][_fl] += -1
 
 for db in seqs:
 	for fl in seqs[db]:
 		out = ''
 		for s in seqs[db][fl]:
 			if seqs[db][fl][s][1]:
-				out += seqs[db][fl][s][0] + s
+				out += seqs[db][fl][s][0]
+				for chunk in range(0,len(s),60):
+					out += s[chunk:chunk+60]
 		with open(db+'/'+fl.split('.')[0]+'_UNIQUES.fa','w+') as f:
 			f.write(out)
 
 with open('repeated_seqs_list.tsv','w+') as f:
 	f.write(repeated)
-
-"""
-print(len(seqs))
-for db in seqs:
-	print('\t'+str(len(seqs[db])))
-	for fl in seqs[db]:
-		print('\t\t'+str(len(seqs[db][fl])))
-"""
-"""
-for fl in fls:
-	with open(fl,'r') as f:
-		seqs[fl] = dict()
-		seq = ''
-		for l in f.readlines():
-			if l[0] == '>':
-				if seq != '':
-					seq = seq.upper()
-					if seq in seqs:
-						repeated += fl + '\t' + seqs[fl][seq][0].strip() + '\t' + fl + '\t' + header.strip() + '\n'
-					seqs[fl][seq] = [header, True]
-				header = l
-				seq = ''
-			else:
-				seq += l
-		seq = seq.upper()
-		if seq in seqs:
-			repeated += fl + '\t' + seqs[fl][seq][0].strip() + '\t' + fl + '\t' + header.strip() + '\n'
-		seqs[fl][seq] = [header, True]
-
-for i in range(len(fls)):
-	for s in seqs[fls[i]]:
-		for j in range(len(fls)):
-			if i != j:
-				if s in seqs[fls[j]]:
-					repeated += fls[i] + '\t' + seqs[fls[i]][s][0].strip() + '\t' + fls[j] + '\t' + seqs[fls[j]][s][0].strip() + '\n'
-					if len(seqs[fls[i]]) > len(seqs[fls[j]]):
-						seqs[fls[i]][s][1] = False
-					else:
-						seqs[fls[j]][s][1] = False
-
-for fl in fls:
-	out = ''
-	for s in seqs[fl]:
-		if seqs[fl][s][1] == True:
-			out += seqs[fl][s][0] + s
-	with open(fl.split('.')[0]+'_UNIQUES.tsv','w+') as f:
-		f.write(out)
-
-with open('repeated_list.txt','w+') as f:
-	f.write(repeated)
-"""
